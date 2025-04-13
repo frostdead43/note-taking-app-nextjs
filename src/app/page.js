873c30bea00b5e9e56be303822c7c7023acfb3e4
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2025-04-13 19:55:13 +0300

    added some styyles

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
-import Link from "next/link"
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css"
 
 
 export default function Home() {
+const [isEmpty,setIsEmpty] = useState(true);
+
   return(
-   <></>
+   <div className ="container">
+    <h2>All Notes</h2>
+    {isEmpty && (
+      <div className={styles["empty-area"]}>
+        <p>You don’t have any notes yet. Start a new note to capture your thoughts and ideas.</p>
+      </div>
+    )}
+    <img className={styles.plus} src="/images/plus.svg"/>
+   </div>
   )
 }
