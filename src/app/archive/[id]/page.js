ee2afd1c93458953573def280b75125df095eda5
“Gökdeniz <gokdenizkeeles@gmail.com>
2025-04-27 20:36:41 +0300

    added unarchive

--- src/app/archive/[id]/page.js
+++ src/app/archive/[id]/page.js
@@ -2,31 +2,119 @@
 import { supabase } from "@/src/app/lib/supabaseClient";
 import "./archiveDetail.css"
 import Link from "next/link";
-import { use,useEffect, useState } from "react";
+import { use, useEffect, useState, useRef } from "react";
+import { redirect } from "next/navigation";
 
 
-export default function Page({params}) {
+
+export default function Page({ params }) {
   const unwrappedArchiveParams = use(params);
   const { id } = unwrappedArchiveParams;
 
-  const [archiveDetail,setArchiveDetail] = useState(null);
+  const [archiveDetail, setArchiveDetail] = useState(null);
+  const dialogRef = useRef(null);
+  const archiveRef = useRef(null);
+
 
   useEffect(() => {
-    async function getArchiveData(){
+    async function getArchiveData() {
       const { data } = await supabase.from("notes").select("*").eq("id", id).single();
       setArchiveDetail(data);
       console.log(data);
     }
     getArchiveData();
-  },[])
+  }, [])
 
+  function handleOpenModal() {
+    dialogRef.current.showModal();
+  }
 
-  return(
+  function handleCloseModal() {
+    dialogRef.current.close();
+  }
+
+  async function handleDelete(id) {
+    const { error } = await supabase.from('notes').delete().eq('id', id);
+    if (error) {
+      console.error(error)
+    } else {
+      redirect("/");
+    }
+  }
+
+  async function handleArchive(id) {
+    const { error } = await supabase.from('notes').update({ archived: false }).eq('id', id);
+
+    if (error) {
+      console.error('Arşive gönderme hatası:', error);
+    } else {
+      console.log('Not başarıyla arşivden çıkarıldı!');
+      redirect("/");
+    }
+  }
+
+  function handleOpenArchiveModal() {
+    archiveRef.current.showModal();
+  }
+
+  function handleCloseArchiveModal() {
+    archiveRef.current.close();
+  }
+
+  return (
     <>
-    <h2>deneme detay</h2>
-    <h6>{archiveDetail?.title}</h6>
+      <form >
+        <div className="detail-container">
+          <div className="menu-bar">
+            <div>
+              <Link href={"/"}><button type="button">Go Back</button></Link>
+            </div>
+            <div className="flex">
+              <img onClick={handleOpenModal} src="../images/delete-icon.svg" />
+              <img onClick={handleOpenArchiveModal} src="../images/archive-icon.svg" />
+              <Link href={"/"}><button type="button">Cancel</button></Link>
+              <button type="submit" className="save-btn">Save Note</button>
+            </div>
+          </div>
+          <div className="body">
+            <input className="title-input" type="text" name="title"  ></input>
+            <div className="new-note-body">
+              <div className="note-input">
+                <img src="../images/tag-mini.svg" />
+                <h6>Tags</h6>
+                <h6 style={{ color: "rgba(51, 92, 255, 1)" }}>{archiveDetail?.tags}</h6>
+              </div>
+              <div className="note-input">
+                <img src="../images/clock-icon.svg" />
+                <h6>Last edited</h6>
+                <h6 style={{ color: "rgba(51, 92, 255, 1)" }}>{archiveDetail?.created_at}</h6>
+              </div>
+            </div>
+            <hr />
+            <div className="asd">
+              <textarea disabled></textarea>
+            </div>
+          </div>
+        </div>
+      </form>
+      <dialog ref={dialogRef}>
+        <h3>Delete Note</h3>
+        <p>Are you sure you want to permanently delete this note? This action cannot be undone.</p>
+        <div className="modal-buttons">
+          <button onClick={handleCloseModal}>Cancel</button>
+          <button onClick={() => handleDelete(id)}>Delete Note</button>
+        </div>
+      </dialog>
+      <dialog ref={archiveRef}>
+        <h3>Archive Note</h3>
+        <p>Are you sure you want to archive this note? You can find it in the Archived Notes section and restore it anytime.</p>
+        <div className="modal-buttons">
+          <button onClick={handleCloseArchiveModal}>Cancel</button>
+          <button onClick={() => handleArchive(id)} style={{ background: "rgba(51, 92, 255, 1)" }} className="archive-modal-button">Archive Notes</button>
+        </div>
+      </dialog>
     </>
-    
+
 
   )
 }
