b816c9214512eecf7240ea6865665e8662badfe8
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2025-04-28 18:26:13 +0300

    search area finished

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,7 +1,64 @@
+"use client";
+import { useEffect, useState } from "react";
+import "./search.css"
+import Link from "next/link";
+import { supabase } from "../lib/supabaseClient";
+
 export default function Search() {
+
+  const [searchNotes, setSearchNotes] = useState([]);
+  const [search,setSearch] = useState('');
+
+  useEffect(()=>{
+    async function getSearchData() {
+      const { data } = await supabase.from('notes').select("*");
+      setSearchNotes(data);
+      console.log(data);
+    }
+    getSearchData();
+  },[]);
+
+
+  function handleSearch(e) {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const lowerSearch = search.toLocaleLowerCase("tr");
+
+  const filterSearch = searchNotes.filter(x => {
+    const find = x.title.toLocaleLowerCase("tr").includes(lowerSearch) || x.tags.toLocaleLowerCase("tr").includes(lowerSearch) || x.created_at.toLocaleLowerCase("tr").includes(lowerSearch);
+  
+    console.log(find);
+    return find;
+  });
+
+
+ 
+
+
   return(
     <>
-    <h2>search</h2>
+    <div className="search-area">
+      <h2>Search</h2>
+      <input type="text" name="search" placeholder="Search..." onChange={handleSearch} />
+      <p>
+        All notes matching <span>{search}</span> are displayed below.
+      </p>
+      <div className="search">
+        {filterSearch.length > 0 ? (
+          filterSearch.map(x=> (
+            <div>
+              <h3>{x.title}</h3>
+              <h5>{x.tags}</h5>
+              <h6>{x.created_at}</h6>
+            </div>
+          ))
+        ):(
+          <h5 className="notFound-text">No notes match your search. Try a different keyword or create a new note.</h5>
+        )}
+      </div>
+    </div>
     </>
   )
 }
