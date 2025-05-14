ca31e9ae412ada78b09b4d1a5af649cabdeef808
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2025-05-15 00:32:16 +0300

    styles added

--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -7,7 +7,7 @@ import { usePathname } from "next/navigation";
 const pageVariants = {
   initial: { opacity: 0, y: -20 },
   animate: { opacity: 1, y: 0, transition: { duration: 0.4 } },
-  exit: { opacity: 0, y: 20, transition: { duration: 0.3 } },
+  exit: { opacity: 0, y: 0, transition: { duration: 0.4 } },
 };
 
 export default function PageWrapper({ children }) {
